abe74c5d6490979afa425421b152cd3cc792b9c0
Andrei Stefan <andrei.stefan@wizant.com>
2013-05-21 16:35:22 +0300

    move active calendars to .dataCache

--- js/application.js
+++ js/application.js
@@ -56,7 +56,9 @@ saturnApp.config(['$routeProvider', function($routeProvider) {
             'baseURL': 'https://www.googleapis.com/calendar/v3'
         };
 
-        $rootScope.dataCache = {};
+        $rootScope.dataCache = {
+            'activeCalendars': []
+        };
 
         $rootScope.user = {
             'loggedIn': false
@@ -248,7 +250,7 @@ saturnApp.controller('EventController', function($scope, $rootScope, $filter, Ev
     $scope.eventSources = [$scope.events];
 
     function listEvents(){
-        angular.forEach($rootScope.activeCalendars, function(value, key){
+        angular.forEach($rootScope.dataCache.activeCalendars, function(value, key){
             var promise = Events.list({
                 'calendarId': value,
                 'access_token': $rootScope.dataCache.access_token
@@ -465,12 +467,10 @@ saturnApp.controller('EventController', function($scope, $rootScope, $filter, Ev
 /******************************************************************/
 /* Calendars */
 saturnApp.controller('CalendarController', function($scope, $rootScope, CalendarList, Calendars){
-    $rootScope.activeCalendars = [];
-
     function initActiveCalendars(){
         angular.forEach($rootScope.dataCache.CalendarList.items,function(value, key){
-            if($rootScope.dataCache.CalendarList.items[key].selected === true && $rootScope.activeCalendars.indexOf($rootScope.dataCache.CalendarList.items[key].id) === -1) {
-                $rootScope.activeCalendars.push($rootScope.dataCache.CalendarList.items[key].id);
+            if($rootScope.dataCache.CalendarList.items[key].selected === true && $rootScope.dataCache.activeCalendars.indexOf($rootScope.dataCache.CalendarList.items[key].id) === -1) {
+                $rootScope.dataCache.activeCalendars.push($rootScope.dataCache.CalendarList.items[key].id);
             }
         });
     }
@@ -483,10 +483,10 @@ saturnApp.controller('CalendarController', function($scope, $rootScope, Calendar
         var calendarID = this.calendar.id,
             selected = this.calendar.selected;
 
-        if(selected && $rootScope.activeCalendars.indexOf(calendarID) === -1){
-            $rootScope.activeCalendars.push(calendarID);
+        if(selected && $rootScope.dataCache.activeCalendars.indexOf(calendarID) === -1){
+            $rootScope.dataCache.activeCalendars.push(calendarID);
         } else {
-            $rootScope.activeCalendars.splice(calendarID, 1);
+            $rootScope.dataCache.activeCalendars.splice(calendarID, 1);
         }
 
         $rootScope.$broadcast('calendar:CalendarListUpdated');
