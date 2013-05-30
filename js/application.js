06d92f5ceead7871721de1cb4a10bdf949cc7077
Andrei Stefan <andrei.stefan@wizant.com>
2013-05-30 19:24:59 +0300

    change colorpicker

--- js/application.js
+++ js/application.js
@@ -51,7 +51,7 @@
                         templateUrl: 'partials/calendar-settings.html'
                     }).
                     when('/calendar/create', {
-                        templateUrl: 'partials/calendar-settings.html'
+                        templateUrl: 'partials/create-calendar.html'
                     }).
                     otherwise({
                         redirectTo: '/'
@@ -274,11 +274,62 @@
     /******************************************************************/
     /* Events */
     saturnApp.controller('EventController', function ($scope, $rootScope, $filter, $location, Events, Calendars, CalendarList) {
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        $scope.evt = [
+            {
+                title: 'All Day Event',
+                start: new Date(y, m, 1)
+            },
+            {
+                title: 'Long Event',
+                start: new Date(y, m, d-5),
+                end: new Date(y, m, d-2)
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d-3, 16, 0),
+                allDay: false
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d+4, 16, 0),
+                allDay: false
+            },
+            {
+                title: 'Meeting',
+                start: new Date(y, m, d, 10, 30),
+                allDay: false
+            },
+            {
+                title: 'Lunch',
+                start: new Date(y, m, d, 12, 0),
+                end: new Date(y, m, d, 14, 0),
+                allDay: false
+            },
+            {
+                title: 'Birthday Party',
+                start: new Date(y, m, d+1, 19, 0),
+                end: new Date(y, m, d+1, 22, 30),
+                allDay: false
+            },
+            {
+                title: 'Click for Google',
+                start: new Date(y, m, 28),
+                end: new Date(y, m, 29),
+                url: 'http://google.com/'
+            }
+        ];
         $scope.events =  function(start, end, callback) {
             return fetchEvents($rootScope.dataCache.calendars, start, end, callback);
         };
 
-        $scope.eventSources = [$scope.events];
+        $scope.eventSources = [$scope.evt, $scope.events];
 
         //fetch events
         var i = 0,
